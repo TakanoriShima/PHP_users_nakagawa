62f9fcff0246d068c5c2912faf348cc47d0cfa3b
TakanoriShima <quark2galaxy@gmail.com>
2022-03-29 12:18:13 +0900

    新規ユーザー登録途中

---
+++ create.php
@@ -0,0 +1,10 @@
+<?php
+    // (C)
+    // セッション開始
+    session_start();
+    // セッションからエラーメッセージを取得
+    $errors = $_SESSION['errors'];
+    // セッション情報の破棄
+    $_SESSION['errors'] = null;
+    // $name = $_POST['name'];
+    include_once 'views/create_view.php';
